7cc191a4113b0141f7a702ac75d2e23b7783932b
Rasul Azimov <r.azimov2005@gmail.com>
2025-05-18 15:55:53 +0300

    картинки и сообщения

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,21 +1,24 @@
+//работа с сервером подключение и получение сообщений с сервера
 const socket = new WebSocket('wss://rasulhub.fun/ws/')
 socket.addEventListener('message', (event) => {
   const message = JSON.parse(event.data)
   console.log(message);
-  if (message.type === 'chat_history') {
-
-  }
-  if (message.type === 'chat_message') {
+  if (message.type === 'new_message') {
     addMessage(message.data);
   }
 })
 
-let userName = 'noName';
+function sendMessage(jsonObject) {
+  socket.send(JSON.stringify(jsonObject));
+}
+setInterval(sendMessage, 30000, { type: 'ping' })
+
+let imageBase64;
 
 const button = document.querySelector(".button");
 if (button) {
   button.addEventListener("click", () => {
-    userName = document.getElementById('username').value;
+    const userName = document.getElementById('username').value;
     localStorage.setItem("username", userName);
     window.location.href = "/pages/chat.html";
   });
@@ -29,6 +32,30 @@ if (button2) {
   });
 }
 
+const photo = document.querySelector('.photo');
+if (photo) {
+  photo.addEventListener('change', () => {
+    const file = photo.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = e => {
+      const img = new Image();
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = 300;
+        canvas.height = 300;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, 300, 300);
+
+        imageBase64 = canvas.toDataURL('image/png').split(',')[1];
+      };
+      img.src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  });
+
+}
 
 const sendButton = document.querySelector('.sendButton');
 if (sendButton) {
@@ -36,17 +63,32 @@ if (sendButton) {
     const inputMessage = document.querySelector('.inputMessage');
     let val = inputMessage.value;
     if (val) {
-      socket.send(JSON.stringify({ type: 'new_message', name: localStorage.getItem("username"), text: val }))
+      sendMessage({
+        type: 'new_message',
+        data: {
+          name: localStorage.getItem("username"),
+          text: val,
+          imageData: imageBase64,
+        }
+      })
+
       inputMessage.value = '';
     }
   });
-
 }
 
 function addMessage(message) {
   const chat = document.querySelector('.chat');
   let div = document.createElement('div');
+  let img = document.createElement('img');
+  img.src = `data:image/png;base64,${message.imageData}`;
   div.className = 'message';
   div.innerHTML = `<b style="color:black">${message.name}</b>:<br /> ${message.text}`;
+  div.appendChild(img);
   chat.appendChild(div);
+  chat.scrollTop = chat.scrollHeight;
 }
+
+
+
+
